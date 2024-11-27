3fc91fd842d599c6d4dfa67cd7b210583b7c1977
Kamran0504 <kamrannutbook05@gmail.com>
2024-11-27 21:13:04 +0500

    HomeKit

--- src/page/home/components/hero/hero.jsx
+++ src/page/home/components/hero/hero.jsx
@@ -13,7 +13,7 @@ export function Hero() {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 2000,
         arrows: false,
         customPaging: (i) => (
